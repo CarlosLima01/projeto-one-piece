4bb9c0c51cf91a5dc94276bec6b8159ab449718d
Carlos Lima <139026273+CarlosLima01@users.noreply.github.com>
2023-10-08 23:29:55 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+const botoes = document.querySelectorAll('.botao');
+
+botoes.forEach((botao, indice) => {
+    botao.addEventListener("click", () => {
+
+        const botaoSelecionado = document.querySelector(".botao.selecionado");
+        botaoSelecionado.classList.remove("selecionado");
+
+        botao.classList.add("selecionado");
+
+        const personagens = document.querySelectorAll(".personagem");
+        const personagemSelecionado = document.querySelector(".personagem.selecionado");
+        personagemSelecionado.classList.remove("selecionado");
+        personagens[indice].classList.add("selecionado");
+    });
+});
